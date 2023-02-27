ee2fa29c0a488a0630ee7318b99a0146778bc4b6
Jacob Rubino <jacobrubino@gmail.com>
2023-02-27 17:45:02 -0500

    had it make sure you picked a character type

--- passwordgenerator.js
+++ passwordgenerator.js
@@ -1,13 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var leng = 0
-
+var passStr = ""
+var passwordText = document.querySelector("#password");
 function passwordLen() {
   leng = window.prompt("Please enter A password length between 8 and 128");
   while (isNaN(leng) || leng < 8 || leng > 128) {
     leng = window.prompt("Not a valid password length, must be between 8 and 128 characters");
   }
-  return leng;
+  return leng
 }
 
 function ifUpper() {
@@ -16,7 +16,7 @@ function ifUpper() {
     useUpper = true;
   } else if(upperInput.toLowerCase()==="no"){
     useUpper = false;
-  }else {return;}
+  }else {useUpper=false;}
 }
 
 function ifLower() {
@@ -25,7 +25,7 @@ function ifLower() {
     useLower = true;
   } else if(upperInput.toLowerCase()==="no"){
     useLower = false;
-  }else {return;}
+  }else {useLower=false;}
 }
 function ifSpec() {
   upperInput = window.prompt(
@@ -35,7 +35,7 @@ function ifSpec() {
     useSpec = true;
   } else if(upperInput.toLowerCase()==="no"){
     useSpec = false;
-  }else {return;}
+  }else {useSpec = false;}
 }
 
 function ifNum() {
@@ -44,23 +44,44 @@ function ifNum() {
     useNum = true;
   } else if(upperInput.toLowerCase()==="no"){
     useNum = false;
-  }else {return;}
+  }else {useNum=false;}
+}
+
+function call_func(){  
+  console.log(passStr)
+  ifUpper();
+  if (useUpper == true) {
+    passStr = passStr.concat(uppy);
+    console.log(passStr)
+  }
+  ifLower();
+  if (useLower == true) {
+    passStr = passStr.concat(charray);
+    console.log(passStr)
+  }
+  ifNum();
+  if (useNum == true) {
+    passStr = passStr.concat(numnum);
+    console.log(passStr)
+  }
+  ifSpec();
+  if (useSpec == true) {
+    passStr = passStr.concat(specc);
+    console.log(passStr)
+  }  
 }
 
 // Write password to the #password input
 function writePassword() {
-  charray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  numnum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  uppy = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  specc = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
-  "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
-  var passStr = "";
-  var passwordText = document.querySelector("#password");
-  var lenPass = passwordLen();
-  var letterPass = "";
-  ifUpper();
+  var password = ''
+  charray = "abcdefghijklmnopqrstuvwxyz";
+  numnum = "1234567890";
+  uppy = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  specc = ["!\#$%&\'()*+,./\\:;<>=?@[]^_`{}|~"]; 
+  var leng = passwordLen();
+  var letterOfPass = "";
+  
+  ifUpper()
   if (useUpper == true) {
     passStr = passStr.concat(uppy);
   }
@@ -76,16 +97,32 @@ function writePassword() {
   if (useSpec == true) {
     passStr = passStr.concat(specc);
   }
-  for (let index = 0; index < lenPass.length; index++) {
-    letterPass = Math.floor(Math.random() * passStr.length);
-    password = password + passStr[letterPass];
-    console.log(passStr)
+
+  while (useSpec == false && useNum == false && useUpper == false && useLower == false){
+    console.log(useLower)
+    console.log(useNum)
+    console.log("weewee")
+    alert("Didnt pick a character type")
+    call_func()
   }
-  passwordText.value
+  console.log(passwordText)
+  console.log(passwordText.value)
+ 
+  for (let index = 0; index < leng; index++) {
+    letterOfPass = Math.floor(Math.random() * passStr.length);
+    console.log(passStr[letterOfPass])
+    password = password + passStr[letterOfPass];
+  }
+  console.log(password)
+  
   passwordText.value = password;
 }
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function(){
+  passwordText.value = ''
+  passStr = ''
+  writePassword()
+});
